d89cdf6f0bd5ca17ed64f77d6c487f7d7909c54a
JulienLuxx <JulienLux@outlook.com>
2019-09-06 19:59:13 +0800

    AddMoqEFfuncForXunit

---
+++ Test.Core/UnitTest/UnitTestAsyncEnumerable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Test.Core
+{
+    public class UnitTestAsyncEnumerable<T> : EnumerableQuery<T>, IAsyncEnumerable<T>, IQueryable<T>
+    {
+        public UnitTestAsyncEnumerable(IEnumerable<T> enumerable) : base(enumerable)
+        { }
+
+        public UnitTestAsyncEnumerable(Expression expression) : base(expression)
+        { }
+
+        public IAsyncEnumerator<T> GetEnumerator()
+        {
+            return new UnitTestAsyncEnumerator<T>(this.AsEnumerable().GetEnumerator());
+        }
+
+        IQueryProvider IQueryable.Provider
+        {
+            get { return new UnitTestAsyncQueryProvider<T>(this);  }
+        }
+    }
+}
