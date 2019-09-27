5ec39f9bff1ae342f2b07d2b7b935bee53de7252
JulienLuxx <JulienLux@outlook.com>
2019-09-27 11:53:40 +0800

    TryBuildBaseUnitTest

--- UnitTest/Test.XUnitTest/BaseUnitTest.cs
+++ UnitTest/Test.XUnitTest/BaseUnitTest.cs
@@ -1,9 +1,12 @@
 ﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Test.Domain;
 using Test.Service.Infrastructure;
+using Test.Service.IOC;
 
 namespace Test.XUnitTest
 {
@@ -17,6 +20,12 @@ namespace Test.XUnitTest
         {
             _serviceCollection = new ServiceCollection();
             _serviceCollection.AddAutoMapper(typeof(CustomizeProfile));
+            _serviceCollection.AddAllSvc();
+        }
+
+        protected void AddDbContext()
+        {
+            _serviceCollection.AddDbContext<TestDBContext>(options => options.UseNpgsql("Host=47.244.228.240;Port=5233;Database=TestDB;Username=root;Password=2134006;"));
         }
 
         protected void BuilderServiceProvider()
--- UnitTest/Test.XUnitTest/TmpUnitTest.cs
+++ UnitTest/Test.XUnitTest/TmpUnitTest.cs
@@ -2,17 +2,27 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Test.Core.IOC;
+using Test.Domain;
 using Test.Service.Impl;
 using Test.Service.Interface;
+using Xunit;
 
 namespace Test.XUnitTest
 {
     public class TmpUnitTest : BaseUnitTest
     {
         private IUserSvc _userSvc { get; set; }
-        public TmpUnitTest() : base() 
+        public TmpUnitTest() : base()
         {
-            _serviceCollection.AddScoped<IUserSvc, UserSvc>();
+            _serviceCollection.AddEncryptUtil();
+            AddDbContext();
+            BuilderServiceProvider();
+            _userSvc = _serviceProvider.GetService<IUserSvc>();
         }
+
+        [Fact]
+        public void Test()
+        { }
     }
 }
